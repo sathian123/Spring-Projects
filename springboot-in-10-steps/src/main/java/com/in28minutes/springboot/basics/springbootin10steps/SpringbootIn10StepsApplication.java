87c5fd07955a541fd1e25113ab3ffde69558108c
sathian123 <38859861+sathian123@users.noreply.github.com>
2021-02-20 11:04:28 +0530

    spring boot in 10 steps

---
+++ springboot-in-10-steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -0,0 +1,20 @@
+package com.in28minutes.springboot.basics.springbootin10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringbootIn10StepsApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+
+//		for (String name : context.getBeanDefinitionNames()) {
+//
+//			System.out.println(name);
+//
+//		}
+	}
+
+}
